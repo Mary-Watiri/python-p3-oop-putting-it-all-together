3aef7664635ba135543ac7d89149432bf4740f56
Mary Watiri <watirimary30@gmail.com>
2024-03-02 18:22:34 +0300

    Completed Assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+        
+    def get_shoe_size(self):
+         return self._size
+     
+    def set_shoe_size(self, size):
+        if(type(size) == int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+    size = property(get_shoe_size, set_shoe_size) 
+    
+    def shoe_repair(self):
+        return ("The shoe has been repaired")
+    
+     
+     
